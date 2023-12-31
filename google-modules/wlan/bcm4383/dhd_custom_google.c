8c1a28bac903569154c0d3c05c476fee74286317
Sultan Alsawaf <sultan@kerneltoast.com>
2023-12-30 19:16:48 -0800

    bcmdhd: Disable IRQ affinity management when SBalance is enabled
    Let SBalance handle IRQ affinities when it's enabled for better efficiency
    and performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcm4383/dhd_custom_google.c
+++ google-modules/wlan/bcm4383/dhd_custom_google.c
@@ -1386,6 +1386,9 @@ void dhd_plat_report_bh_sched(void *plat_info, int resched)
 	uint64 curr_time_ns;
 	uint64 time_delta_ns;
 
+	if (IS_ENABLED(CONFIG_IRQ_SBALANCE))
+		return;
+
 #if !defined(DHD_CUSTOM_PKT_COUNT_ENABLE)
 	if (resched > 0) {
 		resched_streak++;
