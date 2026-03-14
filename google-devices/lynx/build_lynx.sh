53e2daa0c20b0ce5672398c1afff3bc356d5a5c5
Sultan Alsawaf <sultan@kerneltoast.com>
2026-03-14 14:19:32 -0700

    Import 'google-devices/lynx' from android-gs-caimito-6.1-android16-qpr3
    From tarball: kernels-CP1A.260305.018.tar.xz
    Source tree: kernel-caimito-14810637
    
    ☹️
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

---
+++ google-devices/lynx/build_lynx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+source private/devices/google/common/shell_utils.sh
+setup_cog_env_if_needed
+
+exec tools/bazel run \
+    --config=stamp \
+    --config=lynx \
+    //private/devices/google/lynx:gs201_lynx_dist "$@"
