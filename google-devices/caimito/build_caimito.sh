43f366327c77e511d52536fc61ffb40acc91eaa0
Sultan Alsawaf <sultan@kerneltoast.com>
2026-03-14 14:19:32 -0700

    Import 'google-devices/caimito' from android-gs-caimito-6.1-android16-qpr3
    From tarball: kernels-CP1A.260305.018.tar.xz
    Source tree: kernel-caimito-14810637
    
    ☹️
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

---
+++ google-devices/caimito/build_caimito.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+parameters=
+if [ "${BUILD_AOSP_KERNEL}" = "1" ]; then
+  echo "WARNING: BUILD_AOSP_KERNEL is deprecated." \
+    "Use --kernel_package=@//aosp instead." >&2
+  parameters="--kernel_package=@//aosp"
+fi
+
+if [ "${BUILD_STAGING_KERNEL}" = "1" ]; then
+  echo "WARNING: BUILD_STAGING_KERNEL is deprecated." \
+    "Use --kernel_package=@//aosp-staging instead." >&2
+  parameters="--kernel_package=@//aosp-staging"
+fi
+
+source private/devices/google/common/shell_utils.sh
+setup_cog_env_if_needed
+
+exec tools/bazel run \
+    ${parameters} \
+    --config=stamp \
+    --config=caimito \
+    //private/devices/google/caimito:zumapro_caimito_dist "$@"
