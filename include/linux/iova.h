0ctobot/neutrino_kernel_google_caimito

vh: iommu: Eliminate vendor_data dependency from iovad vendor hooks
Add a u64 val member directly into `struct iova_domain` when the iovad
vendor hooks module is enabled to eliminate the vendor_data dependency.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

include/linux/iova.h
@@ -40,6 +40,10 @@ struct iova_domain {
 	struct iova_rcache	*rcaches;
 	struct hlist_node	cpuhp_dead;
 
+#if IS_ENABLED(CONFIG_IOVAD_BEST_FIT_ALGO)
+	u64		val;
+#endif
+
 	ANDROID_VENDOR_DATA(1);
 };
 

troy896/android_kernel_google_tensynos

include/linux/iova.h
@@ -40,6 +40,10 @@ struct iova_domain {
 	struct iova_rcache	*rcaches;
 	struct hlist_node	cpuhp_dead;
 
+#if IS_ENABLED(CONFIG_IOVAD_VENDOR_HOOKS)
+	u64		val;
+#endif
+
 	ANDROID_VENDOR_DATA(1);
 };
 