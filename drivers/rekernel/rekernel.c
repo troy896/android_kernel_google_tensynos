910e7fe3d60b402de93245414c1433aeaa37d798
TYOPXN360 <TYOPXN360@Gmail.com>
2026-06-08 10:19:18 +0800

    fix: rekernel compilation errors
    - Fix format %d -> %lu for unsigned long netlink_unit
    - Change file_operations to proc_ops for newer kernel API
    - Remove extra arguments in snprintf format string

--- drivers/rekernel/rekernel.c
+++ drivers/rekernel/rekernel.c
@@ -187,17 +187,17 @@ struct netlink_kernel_cfg cfg = {
 };
 #ifdef CONFIG_PROC_FS
 static int rekernel_unit_show(struct seq_file* m, void* v) {
-	seq_printf(m, "%d\n", netlink_unit);
+	seq_printf(m, "%lu\n", netlink_unit);
 	return LINE_SUCCESS;
 }
 static int rekernel_unit_open(struct inode* inode, struct file* file) {
 	return single_open(file, rekernel_unit_show, NULL);
 }
-static const struct file_operations rekernel_unit_fops = {
-	.open = rekernel_unit_open,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.release = single_release
+static const struct proc_ops rekernel_unit_fops = {
+	.proc_open = rekernel_unit_open,
+	.proc_read = seq_read,
+	.proc_lseek = seq_lseek,
+	.proc_release = single_release
 };
 #endif /* CONFIG_PROC_FS */
 // init
@@ -222,7 +222,7 @@ static int start_rekernel(void) {
 		pr_err("Failed to create Re:Kernel server!\n");
 		return -LINE_ERROR;
 	}
-	pr_info("Created Re:Kernel server! NETLINK UNIT: %d\n", netlink_unit);
+	pr_info("Created Re:Kernel server! NETLINK UNIT: %lu\n", netlink_unit);
 
 #ifdef CONFIG_PROC_FS
 	rekernel_dir = proc_mkdir("rekernel", NULL);
@@ -230,7 +230,7 @@ static int start_rekernel(void) {
 		pr_err("create /proc/rekernel failed!\n");
 	} else {
 		char buff[32];
-		sprintf(buff, "%d", netlink_unit);
+		sprintf(buff, "%lu", netlink_unit);
 		rekernel_unit_entry = proc_create(buff, 0644, rekernel_dir, &rekernel_unit_fops);
 		if (!rekernel_unit_entry) {
 			pr_err("create rekernel unit failed!\n");
@@ -293,7 +293,7 @@ void rekernel_report(int reporttype, int type, pid_t src_pid, struct task_struct
 			}
 			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=%s,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d,rpc_name=%s,code=%d;", binder_type[type], oneway, src_pid, task_uid(src).val, dst_pid, task_uid(dst).val, buf, tr->code);
 		} else {
-			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=%s,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d;", binder_type[type], oneway, src_pid, task_uid(src).val, dst_pid, task_uid(dst).val, rpc_type[type], -1);
+			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=%s,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d;", binder_type[type], oneway, src_pid, task_uid(src).val, dst_pid, task_uid(dst).val);
 		}
 		break;
 	case SIGNAL:
