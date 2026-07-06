8f4c52b53fa3431451d7fb397e0602062bff1b9c
troy896 <troy896@gmail.com>
2026-07-06 14:02:58 -0500

    update

--- fs/nomount.h
+++ fs/nomount.h
@@ -15,7 +15,7 @@
 #endif
 #include <linux/jump_label.h>
 
-#define NOMOUNT_VERSION   "12"
+#define NOMOUNT_VERSION    "12"
 #define NOMOUNT_HASH_BITS  12
 #define NOMOUNT_SMALL_HASH_BITS 4
 #define NM_FLAG_IS_DIR      (1 << 1)
@@ -47,6 +47,7 @@ struct nm_iop {
     u64 signature;
     struct nomount_rule *rule; 
     struct nomount_dir_node *dir_node;
+    bool is_whiteout;
     bool had_private_flag;
     struct rcu_head rcu;
 };
@@ -106,6 +107,7 @@ struct nomount_rule {
     struct nomount_dir_node *parent_dir;
     struct nomount_dir_node *this_dir;
     struct inode *cached_r_inode;
+    unsigned long v_ino;
     u32 v_hash;
     u16 v_len;
     u8  flags;
@@ -140,6 +142,7 @@ enum {
     NM_CMD_GET_LIST,
     __NM_CMD_MAX,
 };
+#define NOMOUNT_CMD_MAX (__NM_CMD_MAX - 1)
 
 /* Attributes */
 enum {
@@ -194,4 +197,3 @@ enum {
 #endif
 
 #endif /* _LINUX_NOMOUNT_H */
-
--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -303,7 +303,7 @@ static int sel_mmap_handle_status(struct file *filp,
 			       size, vma->vm_page_prot);
 }
 
-const struct file_operations sel_handle_status_ops = {
+static const struct file_operations sel_handle_status_ops = {
 #ifdef CONFIG_KSU_SUSFS
 	.open		= my_sel_open_handle_status,
 #else
