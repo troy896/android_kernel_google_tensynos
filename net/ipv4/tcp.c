108b7fe63748b233f9ca627f7b10184452739820
troy896 <troy896@gmail.com>
2026-08-11 02:12:20 -0500

    remove tcpnodelay fixing bbr3

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3627,9 +3627,6 @@ int do_tcp_setsockopt(struct sock *sk, int level, int optname,
 
 	sockopt_lock_sock(sk);
 
-	/* Hack optname to use TCP_NODELAY for everything */
-	optname=TCP_NODELAY;
-
 	switch (optname) {
 	case TCP_MAXSEG:
 		/* Values greater than interface MTU won't take effect. However
